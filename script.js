1086391a8b66075c374ba64ee871e628a2a95c17
Uriel <uriel_schavez@hotmail.com>
2024-02-19 13:10:54 -0600

    ci: prompt de usuario

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hola Ch38");
+ // Función que envie un saludo con alert.
+  function enviarSaludo(){
+    alert('Hola Ch38');
+ }
+
+ /**
+  * Pedir el nombre de la persona usando prompt()
+  * Mostrar un saludo y nombre d ela persona con alwert()
+  */
+ function enviarSaludoAPersona() {
+    let message = prompt("Hola usuario dime tu nombre");
+    alert('Hola ${message}');
+ }
